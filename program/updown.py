2293a8802e7bd2283ad77ac56f5c3b16d5ebf578
kangge <zhuoyikang@gmail.com>
2017-10-13 21:20:27 +0800

    简单止盈止损计算

---
+++ program/updown.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os, sys
+
+def usage():
+    print "usage: updown.py 买入价格 本金"
+
+
+
+
+if __name__=="__main__":
+    if len(sys.argv) != 3:
+        usage()
+        exit()
+
+
+    price = float(sys.argv[1])
+    principal = float(sys.argv[2])
+    diff = 1000
+    rate = diff / principal
+
+    down = price -  (price * rate)
+    up =  price +  (price * rate * 2)
+
+    print "rate:", rate
+    print "up price:", up
+    print "down price:", down
+
